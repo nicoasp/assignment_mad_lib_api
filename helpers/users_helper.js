8e05912f1ca79d2b9179ef2acf8875ccf3e0abd5
nicoasp <nicolas.amaya@gmail.com>
2017-04-28 18:32:41 +0200

    initial setup

---
+++ helpers/users_helper.js
@@ -0,0 +1,12 @@
+const UsersHelper = {};
+
+
+UsersHelper.usersPath = () => `/users`;
+UsersHelper.userPath = () => `/user`;
+UsersHelper.newUserPath = () => `/user/new`;
+
+
+
+
+
+module.exports = UsersHelper;
